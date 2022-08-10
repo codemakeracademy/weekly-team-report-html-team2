b3fa53ca13290445042cdfc709a1d7656163e449
Efim Shikanov <darkcoderb@gmail.com>
2022-08-10 15:48:11 +0500

    fix: fixed styles and added variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const isProduction = process.env.NODE_ENV == "production";
+const isProduction = process.env.NODE_ENV === "production";
 
 const stylesHandler = "style-loader";
 
@@ -31,25 +31,26 @@ const config = {
       template: "fillOutReport.html",
       filename: "fillOutReport.html",
     }),
-      template: "teamReports.html", 
-      filename: "teamReports.html"
+    new HtmlWebpackPlugin({
+      template: "teamReports.html",
+      filename: "teamReports.html",
     }),
     new HtmlWebpackPlugin({
-      template: "reportHistory.html", 
-      filename: "reportHistory.html"
+      template: "reportHistory.html",
+      filename: "reportHistory.html",
     }),
     new HtmlWebpackPlugin({
-      template: "editCorpInfo.html", 
-      filename: "editCorpInfo.html"
+      template: "editCorpInfo.html",
+      filename: "editCorpInfo.html",
     }),
     new HtmlWebpackPlugin({
       template: "teamMembers.html",
-      filename: "teamMembers.html"
+      filename: "teamMembers.html",
     }),
     new HtmlWebpackPlugin({
       template: "editUserInfo.html",
-      filename: "editUserInfo.html"
-    })
+      filename: "editUserInfo.html",
+    }),
     // Add your plugins here
     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
   ],
@@ -69,8 +70,15 @@ const config = {
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset/resource",
+        type: "asset",
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name][ext]"
+        }
+      }
     ],
   },
 };
